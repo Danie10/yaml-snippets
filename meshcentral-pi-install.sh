8d57281108c42026e4aaae2a66c3864010533e03
Danie <1153726+Danie10@users.noreply.github.com>
2025-12-21 18:53:56 +0200

    Create meshcentral-pi-install.sh

---
+++ meshcentral-pi-install.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install nodejs npm
+mkdir mc
+cd mc
+npm install meshcentral
+nodejs node_modules/meshcentral
+ 
+nodejs node_modules/meshcentral --install
+nodejs install_modules/meshcentral --uninstall
