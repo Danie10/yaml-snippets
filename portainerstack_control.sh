c3d9c81b51edb849c935137c087320d6e4c0e445
Danie <1153726+Danie10@users.noreply.github.com>
2026-03-11 17:32:12 +0200

    Create portainerstack_control.sh

---
+++ portainerstack_control.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Configuration
+PORTAINER_URL="http://192.168.20.2:9000"
+API_TOKEN="redacted"
+ENDPOINT_ID="1" # This is the "1" in your URL #!/1/
+STACK_ID="110"  # This is the Portainer stack "id=xxx" in your URL
+
+# Action should be "start" or "stop"
+ACTION=$1
+
+# Execution
+curl -i -X POST "$PORTAINER_URL/api/stacks/$STACK_ID/$ACTION?endpointId=$ENDPOINT_ID" \
+    -H "X-API-Key: $API_TOKEN" \
+    -H "Content-Type: application/json"
