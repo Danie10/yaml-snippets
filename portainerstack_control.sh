ae6c42adf3928237f9bd3bea3aec6559fa25a35e
Danie <1153726+Danie10@users.noreply.github.com>
2026-03-19 13:18:15 +0200

    Update portainerstack_control.sh

--- portainerstack_control.sh
+++ portainerstack_control.sh
@@ -3,7 +3,7 @@
 # Configuration
 PORTAINER_URL="http://192.168.20.2:9000"
 API_TOKEN="redacted"
-ENDPOINT_ID="1" # This is the "1" in your URL #!/1/
+ENDPOINT_ID="1" # This is the "1" in your Portainer stack URL #!/1/ or 2 if you see #!/2/
 STACK_ID="110"  # This is the Portainer stack "id=xxx" in your URL
 
 # Action should be "start" or "stop"
